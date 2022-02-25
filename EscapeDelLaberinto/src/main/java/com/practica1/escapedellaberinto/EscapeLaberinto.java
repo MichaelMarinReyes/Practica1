6cdccedc34e20de20e83e0c476f4499b255c51d2
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2022-02-24 22:34:55 -0600

    Corrección de mapas y comandos

--- EscapeDelLaberinto/src/main/java/com/practica1/escapedellaberinto/EscapeLaberinto.java
+++ EscapeDelLaberinto/src/main/java/com/practica1/escapedellaberinto/EscapeLaberinto.java
@@ -21,8 +21,8 @@ public class EscapeLaberinto {
 
     public static void menuPrincipal() {
         int opcion;
-        do {
 
+        do {
             System.out.println("\n-Menú Principal-");
             System.out.println("1. Jugar\n2. Crear Mapa\n3. Reportes\n4. Visualizar Mapas\n5. Salir");
             opcion = entrada.nextInt();
@@ -31,21 +31,27 @@ public class EscapeLaberinto {
                 case 1:
                     System.out.println("**********************");
                     jugar();
+
                     break;
 
                 case 2:
                     System.out.println("**********************");
-                    creacionMapas();
+                    tamañoMapa();
+                    diseñoMapa();
+                    mostrarMapa();
+                    menuPrincipal();
                     break;
 
                 case 3:
                     System.out.println("**********************");
                     reporteGeneral();
+                    menuPrincipal();
                     break;
 
                 case 4:
                     System.out.println("**********************");
-                    visualizarMapas();
+                    mapasAlmacenados();
+                    menuPrincipal();
                     break;
 
                 case 5:
@@ -70,19 +76,18 @@ public class EscapeLaberinto {
         char salida = 'S';
         char pared = '#';
 
-        System.out.println("1. Elejir un mapa");
-        System.out.println("2. Regresar al Menu Principal");
+        System.out.println("Elejir un mapa");
+        mostrarMapa();
+        comandos();
+        reportesFinales();
+
+        System.out.println("1. Regresar al Menu Principal");
         subMenu = entrada.nextInt();
 
         if (subMenu == 1) {
-            
-            reportesFinales();
-
-        } else {
             menuPrincipal();
 
         }
-
     }
 
     public static void creacionMapas() {
@@ -93,7 +98,6 @@ public class EscapeLaberinto {
         eleccion = entrada.nextInt();
 
         if (eleccion == 1) {
-            //        mapasAlmacenados();
             tamañoMapa();
 
         } else {
@@ -108,91 +112,126 @@ public class EscapeLaberinto {
 
     }
 
-    public static void mapasAlmacenados(String nombre, int posicionGuardado) {
+    public static void mapasAlmacenados() {
         int indice = 0;
+        int regresar;
         String nombreMapa[] = new String[indice];
         int mapaCreado[] = new int[indice];
 
-        nombre = nombreMapa[indice];
-        posicionGuardado = mapaCreado[indice];
         indice += 1;
 
-        for (int i = 0; i < nombreMapa.length; i++) {
-            System.out.println(nombreMapa[i]);
-        }
+        System.out.println("Regresar al Menú Principal: 1");
+        regresar = entrada.nextInt();
 
-        jugar();
+        if (regresar == 1) {
+            menuPrincipal();
+        } else {
+            mapasAlmacenados();
+        }
     }
+
+    public static String mapas[][][] = new String[30][][];
 
     public static void tamañoMapa() {
         int numeroFilas;
         int numeroColumnas;
+        String nombreMapa;
+        int catidadMapa = 1;
+
+        System.out.print("Escriba un nombre con el que desea guardar el mapa: ");
+        nombreMapa = entrada.next();
 
         System.out.println("Defina el tamaño del laberinto que desea ingresando los siguientes datos:");
         System.out.print("Número de filas: ");
         numeroFilas = entrada.nextInt();
         System.out.print("Número de columnas: ");
         numeroColumnas = entrada.nextInt();
+        System.out.println("");
+        mapas[catidadMapa] = new String[numeroFilas][numeroColumnas];
 
-        int mapa[][] = new int[numeroFilas][numeroColumnas];
-        int posicionGuardado = 1;
-        String nombre;
+    }
 
-        for (int i = 0; i < mapa.length; i++) {
-            for (int j = 0; j < mapa.length; j++) {
-                System.out.println(mapa[i][j]);
+    public static void diseñoMapa() {
+        String jugador = "J";
+        String bot = "B";
+        String casilla = "O";
+        String oro = "G";
+        String salida = "S";
+        String pared = "#";
+        String caracter;
+        int salidas = 0;
+        int tipo;
 
+        for (int i = 0; i < mapas.length; i++) {
+            System.out.println("Fila: " + i);
+            System.out.println("Para crear el mapa de juego indice:\n1.Número de casillas vacias\n2.Oro\n3.Salida\n3.Pared");
+            tipo = entrada.nextInt();
+
+            if (tipo == 1) {
+                caracter = casilla;
+            } else if (tipo == 2) {
+                caracter = oro;
+            } else if (tipo == 3) {
+                caracter = salida;
+            } else {
+                caracter = pared;
+            }
+
+            for (int j = 0; j < mapas[i].length; j++) {
+                for (int k = 0; k < mapas.length; k++) {
+                    mapas[i][j][k] = caracter;
+
+                }
+            }
+        }
+    }
+
+    public static void mostrarMapa() {
+        for (int i = 0; i < mapas.length; i++) {
+            for (int j = 0; j < mapas[i].length; j++) {
+                for (int k = 0; k < mapas.length; k++) {
+                    System.out.println(mapas[i][j][k]);
+                }
+                System.out.println("");
             }
             System.out.println("");
         }
-        mapa[numeroFilas][numeroColumnas] = posicionGuardado;
-        posicionGuardado += 1;
-        System.out.print("Escriba un nombre con el que desea guardar el mapa: ");
-        nombre = entrada.next();
-
-        mapasAlmacenados(nombre, posicionGuardado);
-
     }
 
-     public static void visualizarMapas() {
-
-    }
-     /*
     public static void comandos() {
         String comando;
         String inexistente;
 
-        do {
+        //      do {
+        System.out.println("Ingrese uno de los siguientes comandos\nPuede escribir algunos de los siguientes comandos en mayúscula y minúscula");
+        System.out.println("ORO_REQUERIDO = Muestra cantidad de oro que se necesita para las salidas del laberinto");
+        System.out.println("ORO = Muestra la cantidad de oro que posee");
+        System.out.println("MOVER = Para avanzar posiciónes");
+        System.out.println("LEVANTAR = Para levantar el oro encontrado");
+        System.out.println("MIRAR = Muestra la posición del Jugador y los caminos que podría seguir");
+        System.out.println("SALIR = Al llegar a una de las salidas puede ingresar el comando y terminar el juego");
+        comando = entrada.next();
 
-            System.out.println("Ingrese uno de los siguientes comandos\nPuede escribir algunos de los siguientes comandos en mayúscula y minúscula");
-            System.out.println("ORO_REQUERIDO = Muestra cantidad de oro que se necesita para las salidas del laberinto");
-            System.out.println("ORO = Muestra la cantidad de oro que posee");
-            System.out.println("MOVER = Para avanzar posiciónes");
-            System.out.println("LEVANTAR = Para levantar el oro encontrado");
-            System.out.println("MIRAR = Muestra la posición del Jugador y los caminos que podría seguir");
-            System.out.println("SALIR = Al llegar a una de las salidas puede ingresar el comando y terminar el juego");
-            comando = entrada.next();
+        if (comando.equals("ORO_REQUERIDO") || comando.equals("oro_requerido")) {
+            oroRequerido();
 
-            if (comando == ORO_REQUERIDO || comando == oro_requerido) {
-                oroRequerido();
+        } else if (comando.equals("ORO") || comando.equals("oro")) {
 
-            } else if (comando == ORO || comando == oro) {
+        } else if (comando.equals("MOVER") || comando.equals("mover")) {
+            movimiento();
 
-            } else if (comando == MOVER || comando == mover) {
+        } else if (comando.equals("LEVANTAR") || comando.equals("levantar")) {
 
-            } else if (comando == LEVANTAR || comando == levantar) {
+        } else if (comando.equals("MIRAR") || comando.equals("mirar")) {
 
-            } else if (comando == MIRAR || comando == mirar) {
+        } else if (comando.equals("SALIR") || comando.equals("salir")) {
 
-            } else if (comando == SALIR || comando == salir) {
+        } else {
+            System.out.println("Comando ingresado inexistente, vuelva a intentarlo");
 
-            } else {
-                System.out.println("Comando ingresado inexistente, vuelva a intentarlo");
+        }
 
-            }
-
-        } while (comando != inexistente);
-
+        //     } while (comando = );
     }
 
     public static void oroRequerido() {
@@ -203,27 +242,30 @@ public class EscapeLaberinto {
     }
 
     public static void movimiento() {
-        char direccion;
+        String direccion;
 
-        System.out.println("Para moverse a alguna dirección debe escribir MOVER ó mover seguidamente de la dirección que desea tomar");
-        System.out.println("MOVER A");
-        System.out.println("Para moverse una casilla hacia arriba puede escribir A ó a");
-        System.out.println("Para moverse una casilla hacia arriba puede escribir A ó a");
-        System.out.println("Para moverse una casilla hacia la derecha puede escribir E ó e");
-        System.out.println("Para moverse una casilla hacia la izquierda puede escribir O u o");
-        direccion = entrada.next().charAt(0);
+        System.out.println("Para moverse a alguna dirección debe escribir alguno de los siguentes comandos");
+        System.out.println("Para moverse una casilla hacia arriba puede escribir MOVER A ó mover a");
+        System.out.println("Para moverse una casilla hacia abajo puede escribir MOVER S ó mover s");
+        System.out.println("Para moverse una casilla hacia la derecha puede escribir MOVER E ó mover e");
+        System.out.println("Para moverse una casilla hacia la izquierda puede escribir MOVER O u mover o");
+        direccion = entrada.next();
 
-        if (direccion == A || direccion == a) {
+        if (direccion.equals("MOVER A") || direccion.equals("mover a")) {
             //mover arriba
+            System.out.println("J ha sido desplazado una posición arriba");
 
-        } else if (direccion == S || direccion == s) {
+        } else if (direccion.equals("MOVER S") || direccion.equals("mover s")) {
             //mover abajo
+            System.out.println("J ha sido desplazado una posición abajo");
 
-        } else if (direccion == E || direccion == e) {
-            //mover derecha            
+        } else if (direccion.equals("MOVER E") || direccion.equals("mover e")) {
+            //mover derecha
+            System.out.println("J ha sido desplazado una posición a la derecha");
 
-        } else if (direccion == O || direccion == o) {
+        } else if (direccion.equals("MOVER O") || direccion.equals("mover o")) {
             //mover izquierda
+            System.out.println("J ha sido desplazado una posición a la izquierda");
 
         }
     }
@@ -239,7 +281,7 @@ public class EscapeLaberinto {
 
         System.out.println("Oro: " + oroTotal);
     }
-*/
+
     public static void reportesFinales() {
         int oroRecolectado = 0;
         int movimientosRealizados = 0;
